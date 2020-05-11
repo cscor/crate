803713f5e7e904e6eed894d225300db49ef5bddc
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-11 14:03:33 +0200

    Move s3 related repository analyzer tests

--- plugins/es-repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3RepositoryPluginAnalyzerTest.java
+++ plugins/es-repository-s3/src/test/java/org/elasticsearch/repositories/s3/S3RepositoryPluginAnalyzerTest.java
@@ -19,10 +19,8 @@
  * software solely pursuant to the terms of the relevant commercial agreement.
  */
 
-package io.crate.analyze;
+package org.elasticsearch.repositories.s3;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
 
 import java.util.Collections;
@@ -36,24 +34,27 @@ import org.elasticsearch.cluster.metadata.RepositoriesMetaData;
 import org.elasticsearch.cluster.metadata.RepositoryMetaData;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.test.ClusterServiceUtils;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.crate.analyze.AnalyzedCreateRepository;
+import io.crate.analyze.AnalyzedDropRepository;
+import io.crate.analyze.AnalyzedStatement;
 import io.crate.analyze.repositories.RepositoryParamValidator;
+import io.crate.analyze.repositories.TypeSettings;
 import io.crate.data.Row;
-import io.crate.exceptions.RepositoryAlreadyExistsException;
-import io.crate.exceptions.RepositoryUnknownException;
 import io.crate.planner.PlannerContext;
 import io.crate.planner.node.ddl.CreateRepositoryPlan;
 import io.crate.planner.operators.SubQueryResults;
 import io.crate.test.integration.CrateDummyClusterServiceUnitTest;
 import io.crate.testing.SQLExecutor;
 
-public class CreateDropRepositoryAnalyzerTest extends CrateDummyClusterServiceUnitTest {
+public class S3RepositoryPluginAnalyzerTest extends CrateDummyClusterServiceUnitTest {
 
     private SQLExecutor e;
-    private PlannerContext plannerContext;
     private RepositoryParamValidator repositoryParamValidator;
+    private PlannerContext plannerContext;
 
     @Before
     public void prepare() {
@@ -71,7 +72,9 @@ public class CreateDropRepositoryAnalyzerTest extends CrateDummyClusterServiceUn
         ClusterServiceUtils.setState(clusterService, clusterState);
         e = SQLExecutor.builder(clusterService).build();
         plannerContext = e.getPlannerContext(clusterService.state());
-        repositoryParamValidator = new RepositoryParamValidator(Map.of());
+        repositoryParamValidator = new RepositoryParamValidator(
+            Map.of("s3", new TypeSettings(S3Repository.mandatorySettings(), S3Repository.optionalSettings()))
+        );
     }
 
     @SuppressWarnings("unchecked")
@@ -92,96 +95,62 @@ public class CreateDropRepositoryAnalyzerTest extends CrateDummyClusterServiceUn
         }
     }
 
+
     @Test
-    public void testCreateRepository() {
+    public void testCreateS3RepoWithWrongSettings() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("setting 'wrong' not supported");
+        analyze(e, "CREATE REPOSITORY foo TYPE s3 WITH (wrong=true)");
+    }
+
+    @Test
+    public void testCreateS3RepositoryWithAllSettings() {
         PutRepositoryRequest request = analyze(
             e,
-            "CREATE REPOSITORY \"new_repository\" TYPE \"fs\" WITH (" +
-            "   location='/mount/backups/my_backup'," +
-            "   compress=True)");
-        assertThat(request.name(), is("new_repository"));
-        assertThat(request.type(), is("fs"));
-        assertThat(
-            request.settings().getAsStructuredMap(),
-            allOf(
-                hasEntry("compress", "true"),
-                hasEntry("location", "/mount/backups/my_backup"))
-        );
-    }
-
-    @Test
-    public void testCreateExistingRepository() {
-        expectedException.expect(RepositoryAlreadyExistsException.class);
-        expectedException.expectMessage("Repository 'my_repo' already exists");
-        analyze(e, "CREATE REPOSITORY my_repo TYPE fs");
-    }
-
-    @Test
-    public void testDropUnknownRepository() {
-        expectedException.expect(RepositoryUnknownException.class);
-        expectedException.expectMessage("Repository 'unknown_repo' unknown");
-        analyze(e, "DROP REPOSITORY \"unknown_repo\"");
-    }
-
-    @Test
-    public void testDropExistingRepo() {
-        AnalyzedDropRepository statement = analyze(e, "DROP REPOSITORY my_repo");
-        assertThat(statement.name(), is("my_repo"));
-    }
-
-    @Test
-    public void testCreateAzureRepositoryWithAllSettings() {
-        PutRepositoryRequest request = analyze(
-            e,
-            "CREATE REPOSITORY foo TYPE azure WITH (" +
-            "   container='test_container'," +
-            "   base_path='test_path'," +
+            "CREATE REPOSITORY foo TYPE s3 WITH (" +
+            "   bucket='abc'," +
+            "   endpoint='www.example.com'," +
+            "   protocol='http'," +
+            "   base_path='/holz/'," +
+            "   access_key='0xAFFE'," +
+            "   secret_key='0xCAFEE'," +
             "   chunk_size='12mb'," +
             "   compress=true," +
-            "   readonly=true," +
-            "   location_mode='primary_only'," +
-            "   account='test_account'," +
-            "   key='test_key'," +
-            "   max_retries=3," +
-            "   endpoint_suffix='.com'," +
-            "   timeout='30s'," +
-            "   proxy_port=0," +
-            "   proxy_type='socks'," +
-            "   proxy_host='localhost')");
+            "   server_side_encryption=false," +
+            "   buffer_size='5mb'," +
+            "   max_retries=2," +
+            "   use_throttle_retries=false," +
+            "   readonly=false, " +
+            "   canned_acl=false)");
         assertThat(request.name(), is("foo"));
-        assertThat(request.type(), is("azure"));
+        assertThat(request.type(), is("s3"));
         assertThat(
             request.settings().getAsStructuredMap(),
-            allOf(
-                hasEntry("container", "test_container"),
-                hasEntry("base_path", "test_path"),
-                hasEntry("chunk_size", "12mb"),
-                hasEntry("compress", "true"),
-                hasEntry("readonly", "true"),
-                hasEntry("account", "test_account"),
-                hasEntry("key", "test_key"),
-                hasEntry("max_retries", "3"),
-                hasEntry("endpoint_suffix", ".com"),
-                hasEntry("timeout", "30s"),
-                hasEntry("proxy_port", "0"),
-                hasEntry("proxy_type", "socks"),
-                hasEntry("proxy_host", "localhost")
+            Matchers.allOf(
+                Matchers.hasEntry("access_key", "0xAFFE"),
+                Matchers.hasEntry("base_path", "/holz/"),
+                Matchers.hasEntry("bucket", "abc"),
+                Matchers.hasEntry("buffer_size", "5mb"),
+                Matchers.hasEntry("canned_acl", "false"),
+                Matchers.hasEntry("chunk_size", "12mb"),
+                Matchers.hasEntry("compress", "true"),
+                Matchers.hasEntry("endpoint", "www.example.com"),
+                Matchers.hasEntry("max_retries", "2"),
+                Matchers.hasEntry("use_throttle_retries", "false"),
+                Matchers.hasEntry("protocol", "http"),
+                Matchers.hasEntry("secret_key", "0xCAFEE"),
+                Matchers.hasEntry("server_side_encryption", "false"),
+                Matchers.hasEntry("readonly", "false")
             )
         );
     }
 
     @Test
-    public void testCreateAzureRepoWithMissingMandatorySettings() {
+    public void testCreateS3RepoWithMissingMandatorySettings() {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("The following required parameters are missing to" +
-                                        " create a repository of type \"azure\": [key]");
-        analyze(e, "CREATE REPOSITORY foo TYPE azure WITH (account='test')");
+                                        " create a repository of type \"s3\": [secret_key]");
+        analyze(e, "CREATE REPOSITORY foo TYPE s3 WITH (access_key='test')");
     }
 
-    @Test
-    public void testCreateAzureRepoWithWrongSettings() {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("setting 'wrong' not supported");
-        analyze(e, "CREATE REPOSITORY foo TYPE azure WITH (wrong=true)");
-    }
 }
